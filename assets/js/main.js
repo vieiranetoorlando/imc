2ce40eff4360c1b76d4a1687c3af62a5a09a2a0a
Orlando Vieira Neto <vieiranetoorlando@gmail.com>
2024-01-25 19:17:22 -0300

    Construindo o corpo e implementando o algoritmo de cálculo.

---
+++ assets/js/main.js
@@ -0,0 +1,72 @@
+const form = document.querySelector('#formulario');
+
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    const inputPeso = e.target.querySelector('#peso');
+    const inputAltura= e.target.querySelector('#altura');
+
+    const peso = Number(inputPeso.value);
+    const altura = Number(inputAltura.value);
+   
+
+    if (!peso) {
+        setResultado('Peso inválido', false);
+        return;
+    }
+
+    if (!altura) {
+        setResultado('Altura inválido', false);
+        return;
+    }
+
+    const imc = getImc(peso, altura); 
+    const nivelImc =  getNivelImc(imc);
+
+    console.log(imc, nivelImc);
+});
+
+function getNivelImc (imc) {
+    const nivel = ['Abaixo do peso', 'Peso normal', 'Sobrepeso', 'Obesidade grau 1', 'Obesidade grau 2', 'Obesidade grau 3'];
+
+    if (imc >= 39.9) {
+        return nivel[5];
+    }
+    if (imc >=34.9) {
+        return nivel[4];
+    } 
+    if (imc >=29.9) {
+        return nivel[3];
+    }
+    if (imc >=24.9) {
+        return nivel[2];
+    }
+    if (imc >=18.9) {
+        return nivel[1];
+    }
+    if (imc < 18.9) {
+        return nivel[0];
+    } 
+}
+
+function getImc (peso, altura) {
+    const imc = peso / altura ** 2;
+    return imc.toFixed(2);
+
+}
+
+function criaP () {
+    const p = document.createElement('p');
+    return p;
+
+}
+
+function setResultado (msg, isValid) {
+    const resultado = document.querySelector('#resultado');
+    resultado.innerHTML = '';
+
+
+    const p = criaP();
+    p.innerHTML = msg;
+    resultado.appendChild(p);
+
+}
