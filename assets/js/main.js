d2f7d8cdb73a6ac153fb57b77067afd603a2976e
Orlando Vieira Neto <vieiranetoorlando@gmail.com>
2024-01-26 12:24:42 -0300

    Finalizando resultado

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,31 +21,21 @@ form.addEventListener('submit', function (e) {
 
     const imc = getImc(peso, altura); 
     const nivelImc =  getNivelImc(imc);
-
-    console.log(imc, nivelImc);
+    
+    const msg = `Seu IMC é ${imc} (${nivelImc}).`;
+    setResultado(msg, true);
 });
 
 function getNivelImc (imc) {
     const nivel = ['Abaixo do peso', 'Peso normal', 'Sobrepeso', 'Obesidade grau 1', 'Obesidade grau 2', 'Obesidade grau 3'];
 
-    if (imc >= 39.9) {
-        return nivel[5];
-    }
-    if (imc >=34.9) {
-        return nivel[4];
-    } 
-    if (imc >=29.9) {
-        return nivel[3];
-    }
-    if (imc >=24.9) {
-        return nivel[2];
-    }
-    if (imc >=18.9) {
-        return nivel[1];
-    }
-    if (imc < 18.9) {
-        return nivel[0];
-    } 
+    if (imc >= 39.9) return nivel[5];
+    if (imc >=34.9) return nivel[4];
+    if (imc >=29.9) return nivel[3];
+    if (imc >=24.9) return nivel[2];
+    if (imc >=18.9) return nivel[1];
+    if (imc < 18.9) return nivel[0];
+     
 }
 
 function getImc (peso, altura) {
@@ -64,8 +54,14 @@ function setResultado (msg, isValid) {
     const resultado = document.querySelector('#resultado');
     resultado.innerHTML = '';
 
-
     const p = criaP();
+
+    if (isValid) { p.classList.add('paragrafo-resultado');
+
+} else{
+    p.classList.add('bad');
+}
+
     p.innerHTML = msg;
     resultado.appendChild(p);
 
